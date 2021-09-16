57972fed8adf0882d57226de5d2b753013894f5d
Lak3Scap3 <Lak3Scap3@gmail.com>
2021-09-16 23:44:49 +0300

    Fixed staff detection

--- src/api/tasks/AntiBan.java
+++ src/api/tasks/AntiBan.java
@@ -2,9 +2,11 @@ package api.tasks;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 import api.Locations;
 import net.runelite.api.GameState;
+import simple.hooks.wrappers.SimplePlayer;
 import simple.robot.api.ClientContext;
 import simple.robot.utils.ScriptUtils;
 
@@ -13,7 +15,7 @@ public class AntiBan {
 	public List<String> STAFF_NAMES = Arrays.asList("david", "hope", "polar", "spooky", "chaflie", "corey", "klem",
 			"professor oak", "adreas", "pegasus", "perfection", "raids", "setup", "trilobita", "kenzz", "spirit", "leaned",
 			"paine", "h a r r y", "scape", "mamba", "supreme", "isleview", "kenzz", "andy", "fe chaflie", "zaros", "v12",
-			"zachery", "vcx", "hc wizard", "immortal fox", "listen", "niedermayer", "jake", "julia", "harsh", "hans");
+			"zachery", "vcx", "hc wizard", "immortal fox", "listen", "niedermayer", "jake", "julia", "harsh", "hans", "lily", "chriz", "dead");
 
 	private ClientContext ctx;
 	private int staffSecondsCounter;
@@ -23,14 +25,15 @@ public class AntiBan {
 		this.staffSecondsCounter = 0;
 	}
 
-	public boolean staffFound() {
-		return ctx.players.populate().toStream().anyMatch(val -> {
-			return STAFF_NAMES.contains(ScriptUtils.stripHtml(val.getName()).toLowerCase());
-		});
+	public SimplePlayer staffFound() {
+		return ctx.players.populate().filter(player -> STAFF_NAMES.contains(player.getName().toLowerCase())).next();
+//		return ctx.players.populate().toStream().anyMatch(val -> {
+//			return STAFF_NAMES.contains(ScriptUtils.stripHtml(val.getName()).toLowerCase());
+//		});
 	}
 
 	public void waitOutStaff(int seconds, boolean logout) {
-		while (this.staffFound()) {
+		while (this.staffFound() != null) {
 			if (this.staffSecondsCounter >= seconds) {
 				if (logout) {
 					this.ctx.sendLogout();
