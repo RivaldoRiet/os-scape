c4e74682cbcd56cba982d81815e44da7f34dd14e
Lak3Scap3 <Lak3Scap3@gmail.com>
2021-09-18 14:48:52 +0300

    Update StallThiever.java
    Updated to new API.
    Updated paint
    Added BM profit tracking

--- src/StallThiever/StallThiever.java
+++ src/StallThiever/StallThiever.java
@@ -1,12 +1,13 @@
 package StallThiever;
 
-import api.MenuActions;
-import api.Tasks;
+import api.*;
+import api.utils.Bank;
 import api.utils.Utils;
 import simple.hooks.scripts.Category;
 import simple.hooks.scripts.ScriptManifest;
+import simple.hooks.scripts.listeners.InventoryChangeEvent;
+import simple.hooks.scripts.listeners.InventoryChangeListener;
 import simple.hooks.simplebot.ChatMessage;
-import simple.hooks.wrappers.SimpleNpc;
 import simple.hooks.wrappers.SimpleObject;
 import simple.hooks.wrappers.SimplePlayer;
 import simple.robot.script.Script;
@@ -26,21 +27,23 @@ import static simple.hooks.filters.SimpleSkills.Skills.THIEVING;
         servers = {"Osscape"},
         version = "0.1")
 
-public class StallThiever extends Script {
+public class StallThiever extends Script implements InventoryChangeListener {
 
     private long STARTTIME;
-    private int totalStalls = 0, startXp, startLvl;
+    private int totalBM = 0, startXp;
     private SimplePlayer staff = null;
     private boolean avoidStaff = true;
 
-    final private WorldArea stallsArea = Utils.makeArea(3092,3487,3098,3485,0);
+    final private WorldArea stallsArea = Locations.makeArea(3092,3487,3098,3485,0);
 
     @Override
-    public void onChatMessage(ChatMessage chatMessage) {
-        if (chatMessage.getMessage().contains("You attempt to steal")) {
-            totalStalls += 1;
-        }
+    public void onExecute() {
+        Tasks.init(ctx);
+        Camera.setupCameraZoom();
+        this.STARTTIME = System.currentTimeMillis();
+        this.startXp = Tasks.getSkill().getXP(THIEVING);
     }
+
 
     @Override
     public void onProcess() {
@@ -65,72 +68,66 @@ public class StallThiever extends Script {
 
         else if (ctx.inventory.inventoryFull()) {
             ctx.log("Banking");
-            if (ctx.bank.bankOpen()) {
-                ctx.bank.depositInventory();
+            if (Bank.openBank()) {
+                Bank.depositAll();
                 ctx.sleepCondition(() -> ctx.inventory.isEmpty(), 1000);
-            }
-            else {
-                SimpleObject bank = Tasks.getBanking().getBank();
-                if (bank != null) {
-                    bank.click(0);
-                    ctx.sleepCondition(() -> ctx.bank.bankOpen(), 3000);
-                }
             }
         }
 
-        else if (!stallsArea.containsPoint(ctx.players.getLocal().getLocation())) {
-            // Walk to stalls
+        else if (!stallsArea.containsPoint(Locations.getPlayerLocation())) {
             if (ctx.pathing.reachable(stallsArea.randomTile())) {
                 ctx.log("Walking to stalls");
                 ctx.pathing.step(stallsArea.randomTile());
             }
             else {
-                // Tele to edge
                 ctx.log("Teleporting to edge");
                 MenuActions.invoke("Cast","<col=00ff00>Lumbridge Home Teleport</col>",1,57,-1,14286853);
             }
         } else {
-            SimpleObject stall = ctx.objects.populate().filter(Stall.getMaxStall(ctx.skills.level(THIEVING)).getName()).next();
-            if (stall != null) {
-                if (stall.visibleOnScreen() && stall.validateInteractable()) {
-                    ctx.log("Stealing from stall");
-                    stall.click(0);
-                    ctx.sleepCondition(() -> ctx.players.getLocal().isAnimating(), 500);
-                }
-                else {
-                    ctx.log("Something is wrong, stall is not interactable");
-                }
+            SimpleObject stall = Objects.getNearest(Stall.getMaxStall(ctx.skills.level(THIEVING)).getName());
+            if (Objects.isValid(stall)) {
+                ctx.log("Stealing from stall");
+                stall.click(0);
+                ctx.sleepCondition(() -> ctx.players.getLocal().isAnimating(), 500);
             }
         }
     }
 
     @Override
-    public void onExecute() {
-        Tasks.init(ctx);
-        this.STARTTIME = System.currentTimeMillis();
-        this.startXp = Tasks.getSkill().getXP(THIEVING);
-        this.startLvl = Tasks.getSkill().getLvl(THIEVING);
-
-    }
-
-    @Override
     public void onTerminate() {
-        ctx.log(String.valueOf(System.currentTimeMillis() - STARTTIME));
-
+        int gainedXp = Tasks.getSkill().getXP(THIEVING) - this.startXp;
+        ctx.log("Uptime: " + this.ctx.paint.formatTime(System.currentTimeMillis() - STARTTIME));
+        ctx.log("Total XP: " + Utils.formatNumber(gainedXp));
+        ctx.log("Total BP: " + Utils.formatNumber(totalBM));
     }
 
     @Override
     public void paint(Graphics graphics) {
         int gainedXp = Tasks.getSkill().getXP(THIEVING) - this.startXp;
-        int gainedLvl = Tasks.getSkill().getLvl(THIEVING) - this.startLvl;
         long uptime = System.currentTimeMillis() - this.STARTTIME;
         Graphics2D g = (Graphics2D) graphics;
 
         g.drawString("Total XP: " +
                 Utils.formatNumber(gainedXp) + " / " +
                 Utils.formatNumber(this.ctx.paint.valuePerHour(gainedXp, this.STARTTIME)),
-                550, 425);
-        g.drawString("Uptime: " + this.ctx.paint.formatTime(uptime), 550, 450);
+                550, 420);
+        g.drawString("Total BM: " +
+                        Utils.formatNumber(totalBM) + " / " +
+                        Utils.formatNumber(this.ctx.paint.valuePerHour(totalBM, this.STARTTIME)),
+                550, 440);
+        g.drawString("Uptime: " + this.ctx.paint.formatTime(uptime), 550, 460);
 
     }
+
+    @Override
+    public void onChange(InventoryChangeEvent inventoryChangeEvent) {
+        if (inventoryChangeEvent.getItemName().equals("Blood money") && inventoryChangeEvent.getNewQuantity() != 0) {
+            totalBM += inventoryChangeEvent.getQuantityChange();
+        }
+    }
+
+    @Override
+    public void onChatMessage(ChatMessage chatMessage) {
+    }
+
 }
